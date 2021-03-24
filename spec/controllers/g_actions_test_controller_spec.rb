4d4ea51d8628e2f1e7cc1ba69401d2496404f823
jesusmarlima <jesusmarlima@gmail.com>
2021-03-24 15:20:31 -0400

    add folder to ci

--- spec/controllers/g_actions_test_controller_spec.rb
+++ spec/controllers/g_actions_test_controller_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 describe GActionsTestController do
   context 'Exists' do
+
+    it 'have access to the creds' do
+      cred = Rails.application.credentials[:my_key]
+      expect(cred).to eq(2)
+    end
+
     it 'exits and return alive' do
       get :test
       cred = Rails.application.credentials[:my_key]
