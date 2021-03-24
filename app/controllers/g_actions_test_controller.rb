4da1977e519d06e302558adcaac4a15def4ae877
jesusmarlima <jesusmarlima@gmail.com>
2021-03-24 15:16:46 -0400

    force error

--- app/controllers/g_actions_test_controller.rb
+++ app/controllers/g_actions_test_controller.rb
@@ -1,9 +1,15 @@
 class GActionsTestController < ApplicationController
   def test
 
-    folder = Rails.application.credentials[:temp_folder]
-    File.open("#{folder}newfile", 'w+') do |file|
-      file.puts('test')
+    begin
+      folder = Rails.application.credentials[:temp_folder]
+      return false unless File.directory?(folder)
+
+      File.open("#{folder}newfile", 'w+') do |file|
+        file.puts('test')
+      end
+    rescue StandardError => e
+      raise(e)
     end
 
     render json: { response: Rails.application.credentials[:my_key] }
